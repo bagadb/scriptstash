fd40670523f67805dabda6a83f9482d564e16bfb
Shreyas Lolage <shreyas@shreyas.bagad>
2021-07-01 19:45:07 +0530

    FINISHED BETA

--- scriptstash.sh
+++ scriptstash.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-wget -q -O - "https://scriptstash.herokuapp.com/s/$1" | bash
+if [ "$1" = "list" ]; then
+    wget -q -O - "https://scriptstash.herokuapp.com/$1"
+else
+    wget -q -O - "https://scriptstash.herokuapp.com/s/$1" | bash -s $2 $3 $4 $5
+fi
